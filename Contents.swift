031f5d8d751ae4166c9767e1e1c9b573a2401cf9
chris lekkas <chris.lekkas@fiserv.com>
2018-06-08 17:52:43 -0700

    essageInitial Commit for Swift Apprentice Playground

---
+++ Contents.swift
@@ -0,0 +1,95 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+print(str)
+var variableNumber = 42
+var number2 : Int = 33
+number2 = 44
+
+let myAge = 42
+
+var averageAge : Double = 42.0
+
+averageAge = (averageAge  + 30) / 2
+
+let testNumber : Int = 99
+let moduloNumber = testNumber % 2
+
+var answer = 0
+answer += 1
+answer += 10
+answer >> 3
+
+let exercises = 9
+var exercisesSolved = 0
+exercisesSolved += 1
+
+var age = 16
+print(age)
+age = 30
+print(age)
+
+exercisesSolved += 1
+
+let a: Int = 46
+let b: Int = 10
+//1
+let answer1 = (a * 100) + b
+//2
+let answer2 = (a * 100) + (b * 100)
+//3
+let answer3 = (a * 100) + (b / 10)
+
+exercisesSolved += 1
+
+(5 * 3) - ((4 / 2) * 2)
+
+exercisesSolved += 1
+
+let aa: Double = 99.0
+let bb: Double = 10
+
+let average = (aa + bb) / 2
+
+exercisesSolved += 1
+
+let fahrenheit : Double = 70
+
+var celsius = (fahrenheit - 32) / 1.8
+
+exercisesSolved += 1
+
+let position = 10
+
+var row = position / 7
+
+var column = position % 7
+
+print(row)
+print(column)
+
+exercisesSolved += 1
+
+// ***** TYPE CONVERSION
+
+
+var integer = 100
+var decimal = 12.5
+
+integer = Int(decimal)
+
+
+//type inference is when the compiler/language can figure out the type
+
+let age1 = 42
+let age2 = 21
+
+let avg1 = Double((age1 + age2)) / 2 as Double
+
+
+
+
+
+
